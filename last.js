344e06699c4f0f50f3a8ddb3f2a53b81ce834c52
unknown <marieancillaramirez@gmail.com>
2026-02-12 19:16:29 +0800

    music nalng

--- last.js
+++ last.js
@@ -4,9 +4,19 @@ const gif = document.getElementById("gif");
 const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
 
+let clickCount = 0; // Initialize a counter
+
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yaayyy me love youuu!";
-  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  clickCount++; // Increase count by 1 each click
+
+  if (clickCount === 1) {
+    // First click: Show the love
+    question.innerHTML = "Yaayyy me love youuu!";
+    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  } else if (clickCount === 2) {
+    // Second click: Go back to intro
+    window.location.href = "intro.html";
+  }
 });
 
 noBtn.addEventListener("mouseover", () => {
